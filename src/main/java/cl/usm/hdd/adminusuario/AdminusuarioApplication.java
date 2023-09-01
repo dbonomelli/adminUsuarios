0c67e16254e0ad7220093b84c0c346735969ab9f
bonomelli#3726 <diego.munozde@sansano.usm.cl>
2023-09-01 16:04:55 -0400

    initial push

---
+++ src/main/java/cl/usm/hdd/adminusuario/AdminusuarioApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.adminusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminusuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usm/hdd/adminusuario/AdminusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.adminusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
